8919fffe6f67c8e36610cee2b75d3008b2d70bdd
Oscar <s110616038@stu.ntue.edu.tw>
2021-07-31 00:09:24 +0800

    [Cancel]cancel tutorial normalize

--- tutorial.py
+++ tutorial.py
@@ -158,13 +158,13 @@ data_transforms = {
         # transforms.RandomHorizontalFlip(),
 
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
         transforms.Resize([224,224]),
         # transforms.CenterCrop(input_size),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
 

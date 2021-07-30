45245ada7bc8e1d92de2fa677171ad483075350f
Oscar <s110616038@stu.ntue.edu.tw>
2021-07-31 00:06:58 +0800

    [Fix]Fix tutorial

--- tutorial.py
+++ tutorial.py
@@ -161,7 +161,7 @@ data_transforms = {
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(input_size),
+        transforms.Resize([224,224]),
         # transforms.CenterCrop(input_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])

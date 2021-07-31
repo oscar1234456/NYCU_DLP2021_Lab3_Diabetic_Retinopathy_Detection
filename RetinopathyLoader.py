a0f54c3a4bd7b1dd83aeebbdb0038aa97026fc55
Oscar <s110616038@stu.ntue.edu.tw>
2021-07-31 22:31:59 +0800

    [Change]Change res50 normalize

--- RetinopathyLoader.py
+++ RetinopathyLoader.py
@@ -29,8 +29,8 @@ class RetinopathyLoader(Dataset):
                     # transforms.CenterCrop(224),
                     # transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                    # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),
             "test":transforms.Compose(
@@ -38,8 +38,8 @@ class RetinopathyLoader(Dataset):
                     transforms.Resize(224),
                     # transforms.CenterCrop(224),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                    # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),
         }

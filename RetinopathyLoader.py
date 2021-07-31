4c6c63041f08ed1ebf06cb2f16d475eb75aa763b
Oscar <s110616038@stu.ntue.edu.tw>
2021-07-31 16:04:00 +0800

    [Add]Add back normalize

--- RetinopathyLoader.py
+++ RetinopathyLoader.py
@@ -29,7 +29,7 @@ class RetinopathyLoader(Dataset):
                     # transforms.CenterCrop(224),
                     # transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),
@@ -38,7 +38,7 @@ class RetinopathyLoader(Dataset):
                     transforms.Resize(224),
                     # transforms.CenterCrop(224),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),

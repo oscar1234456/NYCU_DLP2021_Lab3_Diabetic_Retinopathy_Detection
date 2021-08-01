fa79cfe16227eb5a56e30bde3600cbbb6b3db6eb
Oscar <s110616038@stu.ntue.edu.tw>
2021-08-01 09:07:49 +0800

    [Modify]cancel all trans except resize, toTensor

--- RetinopathyLoader.py
+++ RetinopathyLoader.py
@@ -24,12 +24,12 @@ class RetinopathyLoader(Dataset):
             "train":transforms.Compose(
                 [
                     # transforms.RandomRotation(degrees=(0,180)),
-                    transforms.RandomResizedCrop(224),
-                    # transforms.Resize(224),
+                    # transforms.RandomResizedCrop(224),
+                    transforms.Resize(224),
                     # transforms.CenterCrop(224),
-                    transforms.RandomHorizontalFlip(),
+                    # transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),
@@ -38,7 +38,7 @@ class RetinopathyLoader(Dataset):
                     transforms.Resize(224),
                     # transforms.CenterCrop(224),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                    # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
                 ]
             ),

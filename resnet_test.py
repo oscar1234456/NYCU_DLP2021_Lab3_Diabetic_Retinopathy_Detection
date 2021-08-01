6fd0c1eef611c61ace2a8a925c481fd165f278f9
Oscar <s110616038@stu.ntue.edu.tw>
2021-08-01 18:38:05 +0800

    [Modify] lr batch platenu resize224 no normalize

--- resnet_test.py
+++ resnet_test.py
@@ -22,15 +22,15 @@ model_name = "resnet50" # [resnet18, resnet50]
 
 num_classes = 5
 
-batch_size = 4
+batch_size = 256
 
-num_epochs = 8
+num_epochs = 20
 
-learning_rate = 0.005
+learning_rate = 0.1
 
 momentum_val = 0.9
 
-weight_decay_val = 1e-4
+weight_decay_val = 5e-4
 # Flag for feature extracting. When False, we finetune the whole model,
 #   when True we only update the reshaped layer params
 feature_extract =False
@@ -44,7 +44,7 @@ def set_parameter_requires_grad(model, feature_extracting):
             param.requires_grad = False
 
 ##
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
+def train_model(model, dataloaders, criterion, optimizer,scheduler ,num_epochs=25):
     since = time.time()
 
     val_acc_history = []
@@ -110,6 +110,8 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=25):
                 train_acc_history.append(epoch_acc)
 
         print()
+        if phase=="val":
+            scheduler.step(epoch_loss)
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
@@ -193,10 +195,10 @@ optimizer_ft = optim.SGD(params_to_update, lr=learning_rate, momentum=momentum_v
 criterion = nn.CrossEntropyLoss()
 
 ## learning rate scheduler
-# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='min', factor=0.1, patience=2)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_ft,factor=0.1, patience=2)
 
 # Train and evaluate (return model, train_acc_history, test_acc_history)
-model_ft, train_hist, test_hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft,num_epochs=num_epochs)
+model_ft, train_hist, test_hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft,scheduler=scheduler,num_epochs=num_epochs)
 
 ## Save my model
 torch.save(model_ft.state_dict(), 'resnet50_weight1.pth')

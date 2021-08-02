2f18fac733415b38f7b6e1d62049afc66a9ebaa4
Oscar <s110616038@stu.ntue.edu.tw>
2021-08-02 13:12:08 +0800

    let it same as test 18:

--- RetinopathyLoader.py
+++ RetinopathyLoader.py
@@ -25,8 +25,8 @@ class RetinopathyLoader(Dataset):
                 [
                     # transforms.RandomRotation(degrees=(0,180)),
                     # transforms.RandomResizedCrop(224),
-                    transforms.Resize(260),
-                    transforms.CenterCrop(224),
+                    transforms.Resize(224),
+                    # transforms.CenterCrop(224),
                     # transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
                     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -38,8 +38,8 @@ class RetinopathyLoader(Dataset):
             ),
             "test":transforms.Compose(
                 [
-                    transforms.Resize(260),
-                    transforms.CenterCrop(224),
+                    transforms.Resize(224),
+                    # transforms.CenterCrop(224),
                     transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
                     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])

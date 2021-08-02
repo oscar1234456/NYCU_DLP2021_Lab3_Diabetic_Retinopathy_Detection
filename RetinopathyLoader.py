77e2a6f77d9cb92cb4aa13ed9e5e3c0986952c9f
Oscar <s110616038@stu.ntue.edu.tw>
2021-08-02 18:51:42 +0800

    [Add]Add horiaontal flip

--- RetinopathyLoader.py
+++ RetinopathyLoader.py
@@ -32,7 +32,7 @@ class RetinopathyLoader(Dataset):
                     # transforms.RandomResizedCrop(224),
                     # transforms.Resize(260),
                     # transforms.CenterCrop(224),
-                    # transforms.RandomHorizontalFlip(),
+                    transforms.RandomHorizontalFlip(),
                     # transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]
                     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                     # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
